cd9754265a0d256452196b5d90145176e9937c67
Pedro-j-code <pessoinhaaa2@gmail.com>
2025-08-20 22:43:34 -0300

    classes

---
+++ gnose3.py
@@ -0,0 +1,41 @@
+class Gnose:
+    obj = []
+
+    def __init__(self, tecido):
+        self.tecido = tecido
+        Gnose.obj.append(self)
+
+    def rasgar(self):
+        print(f"O tecido da realidade foi rasgado em {self.tecido}% ")
+
+    def costurar(self):
+        print(f"O tecido da realidade foi costurado em {self.tecido}% ")
+
+    def excluir(self):
+        Gnose.obj.remove(self)
+        print("Objeto excluído")
+
+    def editar(self, novo_tecido):
+        self.tecido = novo_tecido
+        print(f"Tecido atualizado para {self.tecido}%")
+
+    def detalhes(self):
+        print(f"Objeto com tecido = {self.tecido}%")
+
+    @classmethod
+    def lista(cls):
+        return cls.obj
+
+
+
+g1 = Gnose(200)
+g2 = Gnose(400)
+ 
+g1.rasgar()
+g1.editar(250)
+g1.detalhes()
+g1.costurar()
+
+print("Objetos existentes:", Gnose.lista())
+g2.excluir()
+print("Objetos restantes:", Gnose.lista())
